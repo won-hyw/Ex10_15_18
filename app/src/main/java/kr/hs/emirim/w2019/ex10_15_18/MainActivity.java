212c24b62bfd9e1f0f64166f70d97fbba629632c
choi ji won <w2019@e-mirim.hs.kr>
2021-07-20 13:23:51 +0900

    Edit MainActivity

--- app/src/main/java/kr/hs/emirim/w2019/ex10_15_18/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/ex10_15_18/MainActivity.java
@@ -8,11 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1;
     EditText edit2;
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
         edit1 = findViewById(R.id.edit1);
         edit2 = findViewById(R.id.edit2);
+        rg = findViewById(R.id.rg);
+
         Button btnCalc = findViewById(R.id.btn_calc);
 
         btnCalc.setOnClickListener(btnListener);
@@ -30,9 +34,25 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            char op = 0;
+            switch (rg.getCheckedRadioButtonId()) {
+                case R.id.radio_plus:
+                    op = '+';
+                    break;
+                case R.id.radio_minus:
+                    op = '-';
+                    break;
+                case R.id.radio_multi:
+                    op = '*';
+                    break;
+                case R.id.radio_divide:
+                    op = '/';
+                    break;
+            }
             Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
             intent.putExtra("num1", Integer.parseInt(edit1.getText().toString()));
             intent.putExtra("num2", Integer.parseInt(edit2.getText().toString()));
+            intent.putExtra("op", op);
             startActivityForResult(intent, 0);
         }
     };
